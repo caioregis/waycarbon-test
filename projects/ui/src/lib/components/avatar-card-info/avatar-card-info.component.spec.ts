7b7a520ce7e82e3ccea7caefdad85a5e8799362e
Caio Régis <regisacaio@gmail.com>
2023-03-30 20:24:52 -0300

    avatar-card-info test

--- projects/ui/src/lib/components/avatar-card-info/avatar-card-info.component.spec.ts
+++ projects/ui/src/lib/components/avatar-card-info/avatar-card-info.component.spec.ts
@@ -1,23 +1,48 @@
+import { Component, DebugElement } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 import { AvatarCardInfoComponent } from './avatar-card-info.component';
+
+@Component({
+  template: `
+  <ui-avatar-card-info>
+    <div class="title">TITLE</div>
+    <div class="subtitle">SUBTITLE</div>
+  </ui-avatar-card-info>
+  `,
+})
+class TestHostComponent {}
+
 
 describe('AvatarCardComponent', () => {
   let component: AvatarCardInfoComponent;
   let fixture: ComponentFixture<AvatarCardInfoComponent>;
+  let debugElement: DebugElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ AvatarCardInfoComponent ]
+      imports: [ AvatarCardInfoComponent ],
+      declarations: [ TestHostComponent ]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(AvatarCardInfoComponent);
+    fixture = TestBed.createComponent(TestHostComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    debugElement = fixture.debugElement;
+    console.log(debugElement)
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should create', () => {
+    const titleText = debugElement.query(By.css('[data-testid="title"]'));
+    const subtitleText = debugElement.query(By.css('[data-testid="subtitle"]'));
+    
+    expect(titleText.nativeElement.textContent).toBe('TITLE');
+    expect(subtitleText.nativeElement.textContent).toBe('SUBTITLE');
   });
 });
