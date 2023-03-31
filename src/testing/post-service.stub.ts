9cda29968178321c89a79336bbe620b60216e97e
Caio Régis <regisacaio@gmail.com>
2023-03-30 23:57:24 -0300

    post author test

---
+++ src/testing/post-service.stub.ts
@@ -0,0 +1,12 @@
+import { Subject } from "rxjs";
+import { Post } from "src/app/models/post";
+
+export const postMock = new Subject<Post>();
+
+export const postServiceStub = {
+  post$: postMock.asObservable(),
+  ngOnDestroy: () => {},
+  loadInitialData: () => {},
+  saveComment: () => {},
+  like: () => {},
+};
