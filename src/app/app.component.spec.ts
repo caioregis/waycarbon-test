12ac8bfe28716988300c32cb33b68ec53a5941c8
Caio Régis <regisacaio@gmail.com>
2023-03-31 02:49:15 -0300

    app component test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,12 +2,21 @@ import { TestBed } from '@angular/core/testing';
 import { HeaderComponent } from '@ui';
 import { AppComponent } from './app.component';
 import { RouterTestingModule } from '@angular/router/testing';
+import { ModalService } from 'projects/ui/src/lib/components/modal/modal.service';
+import { ViewContainerRef } from '@angular/core';
+
+const modalServiceStub = {
+  setRootViewContainerRef: () => {},
+};
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [
         AppComponent
+      ],
+      providers: [
+        { provide: ModalService, useValue: modalServiceStub }
       ],
       imports: [RouterTestingModule, HeaderComponent]
     }).compileComponents();
@@ -17,6 +26,16 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
+  });
+
+  it('should setRootViewContainerRef on constructor', () => {
+    const modalService = TestBed.inject(ModalService);
+    const spy = spyOn(modalService, 'setRootViewContainerRef');
+
+    const fixture = TestBed.createComponent(AppComponent);
+    const viewContainerRef = fixture.componentRef.injector.get(ViewContainerRef);
+    
+    expect(spy).toHaveBeenCalledWith(viewContainerRef);
   });
 
 });
